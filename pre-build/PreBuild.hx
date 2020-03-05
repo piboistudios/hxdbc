fe7d4c9786e9406e94258068778c425d2baa1e81
Gabriel A. Hayes <gabriel.speed.bullock@gmail.com>
2020-03-05 14:54:21 -0600

    Test extraParams.hxml

--- pre-build/PreBuild.hx
+++ pre-build/PreBuild.hx
@@ -1,5 +1,26 @@
 class PreBuild {
 	public static function main() {
-		trace("Prebuild", Sys.getCwd());
-	}
+        final hxml = sys.io.File.getContent("./haxe-libraries/hxdbc.hxml");
+        final classPathParser = ~/-cp (.*)/gi;
+        if(classPathParser.match(hxml)) {
+            final path = classPathParser.matched(1);
+            final info = getProjectInfoFromSrcPath(path);
+            trace(haxe.Json.stringify(info));
+        }
+    }
+    static function getProjectInfoFromSrcPath(path) {
+        var retVal:Dynamic = {};
+        var pathParts = haxe.io.Path.normalize(path).split("/");
+        var basePathLength = 4;
+        retVal.projectName = pathParts[1];
+        retVal.projectVer = pathParts[2];
+        retVal.projectLoc = pathParts[3];
+        retVal.gitHash = "";
+        if(projectLoc.toLowerCase() == "github") {
+        	retVal.gitHash = pathParts[4];    
+            basePathLength=5;
+        }
+        retVal.basePath = pathParts.slice(0, basePathLength).join('/');
+        return retVal;
+    }
 }
