63e312f0c8f512f2c8026afe64273114fe04c5a4
Gabriel A. Hayes <gabriel.speed.bullock@gmail.com>
2020-03-05 18:20:04 -0600

    Add prebuild.

--- Main.hx
+++ Main.hx
@@ -1,16 +1,50 @@
 import sys.db.Odbc;
 
 class Main {
-	static var connectionStringsCfg = haxe.Json.parse(sys.io.File.getContent("../../connection-strings.config"));
+	static var testFixtures = haxe.Json.parse(sys.io.File.getContent("../../test-fixtures.config"));
 
 	public static function main():Void {
-		var cnx = Odbc.connect(connectionStringsCfg.test);
-		var results = cnx.request("SELECT TOP(10) * FROM ROADRUNNER..ORDDET");
+		var cnx = Odbc.connect(testFixtures.test);
+		var results = cnx.request(testFixtures.queryOne);
 		for (result in results) {
-			trace(result);
+			trace(result.OrderNo);
 		}
-		cnx.close();
-		trace("Done");
-		// Odbc.test_sql();
+		trace(cnx.parameters);
+		trace(cnx.dbName());
+    Sys.command('nmake /f Makefile.hl.ammer');
+    final vsDevShell = new sys.io.Process('"${getLatestWindowsSDKToolsVersion()}vsdevcmd.bat"');
+    // trace(vsDevShell.stdout.readAll().toString());
+    cnx.close();
+    var buf = new StringBuf();
+    Sys.sleep(3);
+    while(try {
+      buf.addChar(vsDevShell.stdout.readByte());
+      true;
+    } catch(_:Dynamic) { false; } ) {
+
+    }
+    trace('OUTPUT');
+    trace(vsDevShell.exitCode(true));
+    trace(buf.toString());
+    // vsDevShell.kill();
+    trace(vsDevShell.getPid());
 	}
+
+	static function getLatestWindowsSDKToolsVersion() {
+    var vars = Sys.environment();
+    var highestVer = 0;
+    var retVal = "";
+		for (key => value in vars) {
+      final re = ~/VS(\d{2,3})COMNTOOLS/gi;
+			if(re.match(key)) {
+        final ver = Std.parseInt(re.matched(1));
+        if(ver > highestVer){
+          trace('ver: $ver, highest: $highestVer');
+          highestVer = ver;
+          retVal = value;
+        } 
+      }
+    }
+    return retVal;
+  }
 }
--- prebuild/PreBuild.hx
+++ prebuild/PreBuild.hx
@@ -1,27 +1,74 @@
 package prebuild;
+
+using StringTools;
+
 class PreBuild {
-	public static function main() {
-        final hxml = sys.io.File.getContent("./haxe_libraries/hxdbc.hxml");
-        final classPathParser = ~/-cp (.*)/gi;
-        if(classPathParser.match(hxml)) {
+    static var lixLibCachePath = Sys.getEnv("HAXE_LIBCACHE");
+	public static function run(config:Dynamic) {
+        if(lixLibCachePath != null && lixLibCachePath.length != 0) {
+
+            var lixHxmlPath = './${config.haxeLibsDir}/${config.haxelib}.hxml';
+            if (sys.FileSystem.exists(lixHxmlPath)) {
+                config.lixHxmlPath = lixHxmlPath;
+                runLixBuild(config);
+                return;
+            } 
+        }
+        runHaxelibBuild(config);
+		return;
+	}
+
+	static function runLixBuild(config:Dynamic) {
+		final hxml = sys.io.File.getContent(config.lixHxmlPath);
+		final classPathParser = ~/-cp (.*)/gi;
+		if (classPathParser.match(hxml)) {
             final path = classPathParser.matched(1);
-            final info = getProjectInfoFromSrcPath(path);
-            trace(haxe.Json.stringify(info));
-        }
-    }
-    static function getProjectInfoFromSrcPath(path) {
-        var retVal:Dynamic = {};
-        var pathParts = haxe.io.Path.normalize(path).split("/");
-        var basePathLength = 4;
-        retVal.projectName = pathParts[1];
-        retVal.projectVer = pathParts[2];
-        retVal.projectLoc = pathParts[3];
-        retVal.gitHash = "";
-        if(retVal.projectLoc.toLowerCase() == "github") {
-        	retVal.gitHash = pathParts[4];    
-            basePathLength=5;
-        }
-        retVal.basePath = pathParts.slice(0, basePathLength).join('/');
-        return retVal;
-    }
+            try {
+
+                final info = getProjectInfoFromSrcPath(path);
+                build(config, info.basePath);
+            } catch(e:Dynamic) {
+                trace('Could not build ${config.lixHxmlPath}.hxml');
+                trace('ERROR: $e');
+            }
+		}
+	}
+
+	static function runHaxelibBuild(config:Dynamic) {
+		final haxelibPath = Sys.getEnv("HAXELIB_PATH");
+		final thisLibPath = '$haxelibPath/${config.haxelib}';
+		final currentFilePath = '$thisLibPath/.current';
+		if (sys.FileSystem.exists(currentFilePath)) {
+            try {
+
+                build(config, '$thisLibPath/${~/\\./gi.replace(sys.io.File.getContent(currentFilePath), ',')}');
+            } catch(e:Dynamic) {
+                trace('Could not locate \'${config.haxelib}.hxml\' in HAXELIB_PATH.');
+                trace('ERROR: $e');
+            }
+		}
+	}
+
+	static function build(config, libraryPath) {
+		final userCwd = Sys.getCwd();
+		Sys.setCwd(libraryPath + '/${config.nativePath}');
+		new sys.io.Process(config.buildCmd).exitCode(true);
+		Sys.setCwd(userCwd);
+	}
+
+	static function getProjectInfoFromSrcPath(path) {
+		var retVal:Dynamic = {};
+		var pathParts = haxe.io.Path.normalize(path).split("/");
+		var basePathLength = 4;
+		retVal.name = pathParts[1];
+		retVal.version = pathParts[2];
+		retVal.source = pathParts[3];
+		retVal.git = {hash: ""};
+		if (retVal.source.toLowerCase() == "github") {
+			retVal.git.hash = pathParts[4];
+			basePathLength = 5;
+		}
+		retVal.basePath = pathParts.slice(0, basePathLength).join('/').replace("${HAXE_LIBCACHE}", Sys.getEnv("HAXE_LIBCACHE"));
+		return retVal;
+	}
 }
